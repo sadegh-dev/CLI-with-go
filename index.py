b6cfd2a3f32181b86f347fa2d8a0de49fb0633a1
sadegh-dev <sadegh.rez@gmail.com>
2022-11-02 12:25:55 +0330

    oop - abstract methods

---
+++ index.py
@@ -0,0 +1,18 @@
+"""
+[PYTHON]
+
+	#OOP
+		- list class Methods
+		- obj.__dict__
+		- class variable
+		- comparison Methods
+		- Container object
+		- iterable object
+		- method chaning
+		- attributes management
+		- polymorphism
+		- object (object)
+		- abstract class - abstract methods
+
+
+"""
