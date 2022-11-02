24ec61f8526b02fe4dcf8899681470d9fb9654ab
sadegh-dev <sadegh.rez@gmail.com>
2022-11-02 12:54:56 +0330

    end of design patetrns

--- index.py
+++ index.py
@@ -4,15 +4,86 @@
 #OOP
 	- list class Methods
 	- obj.__dict__
-	- class variable
+	- class variables
 	- comparison Methods
 	- Container object
 	- iterable object
 	- method chaning
-	- attributes management
+	+ attributes management
+		- delattr
+		- getattr
+		- hasattr
+		- setattr
+
 	- polymorphism
 	- object (object)
 	- abstract class - abstract methods
+	- inheritance
+	- mixin class
+	- inheritancec from built-in objects
+	- multi inheritance
+	- access levels in class
+	- decorator property
+	- methods types in class
+	+ magic-special methods [built-in] in class
+		- __del__
+		- __init__
+		- __new__
+		- __str__
+		- __repr__
+		- __call__
+		- __add__
+		- __len__
+		- __missing__
+
+		- __getitem__
+		- __setitem__
+		- __delitem__
+
+		- __getattr__
+		- __setattr__
+		- __delattr__
+		- __getattribute__
+
+		- __reversed__
+		- __bool__
+
+	- Meta class
+	- nasted class
+	+ descriptors
+		- @property [getter]
+		- @name.setter
+		- @name.deleter
+
+	+ descriptors
+		- __set_name__
+		- __get__
+		- __set__
+		- __delete__
+
+	- SOLID architecture
+
+
+# design patterns
+	- singleton [creational]
+	- factory [creational]
+	- abstract factory [creational]
+	- prototype [creational]
+	- builder [creational]
+	- adapter [structural]
+	- decorator [structural]
+	- facade [structural]
+	- proxy [structural]
+	- chain of responsibility [behavioral]
+	- strategy [behavioral]
+	- iterator [behavioral]
+	- observer [behavioral]
+	- template methods [behavioral]
+	- state [behavioral]
+	- composite [creational]
+
+
+
 
 
 """
