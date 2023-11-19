4303192a25973d6e1973999734de815ad78bf7c6
sadegh-dev <sadegh.rez@gmail.com>
2023-11-19 10:19:10 +0330

    add scanner for get command from user

--- CLIUsingGolang.go
+++ CLIUsingGolang.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	//"os/exec"
+	//"strings"
 )
 
 func main() {
 	fmt.Println("Cloud CLI")
 	fmt.Println("---------------------")
 	fmt.Println("=>")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		userInput := scanner.Text()
+		fmt.Println(userInput)
+		fmt.Println("=>")
+	}
 }
 
 func execPINGCommand(cmdParam string) string {
